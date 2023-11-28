89b8eeeebf221617302f63a96670bec605eda00b
luidimso <luidi_mso@outlook.com>
2023-11-28 19:15:18 -0300

    Moving loader method to inside of Event component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/Home";
-import Events from "./pages/Events";
+import Events, {loader as eventsLoader} from "./pages/Events";
 import EventDetails from "./pages/EventDetail";
 import NewEvent from "./pages/NewEvent";
 import EditEvent from "./pages/EditEvent";
@@ -36,16 +36,7 @@ const router = createBrowserRouter([
     children: [
       {index: true, element: <HomePage />},
       {path: "events", element: <EventsRootLayout></EventsRootLayout>, children: [
-        {index: true, element: <Events />, loader: async () => {
-          const response = await fetch('http://localhost:8080/events');
-
-          if (!response.ok) {
-            // TO DO
-          } else {
-            const resData = await response.json();
-            return resData.events;
-          }
-        }},
+        {index: true, element: <Events />, loader: eventsLoader},
         {path: ":id", element: <EventDetails />},
         {path: "new", element: <NewEvent />},
         {path: ":id/edit", element: <EditEvent />}
